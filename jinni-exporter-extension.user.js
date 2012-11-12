e039600b3770aff011e0a2e736091558ff26ea0d
idpsycho <dont@mail.me>
2012-11-12 06:52:22 +0100

    first version

---
+++ jinni-exporter-extension.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           Jinni Exporter
+// @include        http://www.jinni.com*
+// @include        https://www.jinni.com*
+// @match          http://www.jinni.com/*
+// @match          https://www.jinni.com/*
+// ==/UserScript==
+
+
+function load(src, fn) { var x=document.createElement('script'); x.type='text/javascript'; x.src=src; if (fn) { x.onreadystate=fn; x.onload=fn; } document.head.appendChild( x ); };
+
+load('http://p.brm.sk/jinni-exporter/jinni-exporter.js', function() { } );
